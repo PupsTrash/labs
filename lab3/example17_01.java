3352870d42771fdc3fd54da15941b528ee9f3379
PupsTrash <891269@mail.ru>
2021-01-04 20:42:28 +0500

    lab7 maybe final add exmpl_01

--- lab3/example17_01.java
+++ lab3/example17_01.java
@@ -1,2 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class example17_01 {
-}
+
+        public static void main(String[] args) throws IOException {
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            switch (bufferedReader.readLine()){
+                case ("1"):
+                    System.out.println("понедельник");
+                    break;
+                case ("2"):
+                    System.out.println("вторник");
+                    break;
+                case ("3"):
+                    System.out.println("среда");
+                    break;
+                case ("4"):
+                    System.out.println("четверг");
+                    break;
+                case ("5"):
+                    System.out.println("пятница");
+                    break;
+                case ("6"):
+                    System.out.println("суббота");
+                    break;
+                case ("7"):
+                    System.out.println("воскресенье");
+                    break;
+                default:
+                    System.out.println("такого дня нет");
+                    break;
+            }
+        }
+    }
